be39998d4b0cd3fb253db6e25282e6ca1fd184c5
martingega <martin.gega@studenti.unimi.it>
2024-02-06 16:53:27 +0100

    implemented more types of pizza

---
+++ src/main/java/org/pizzastore/CheesePizza.java
@@ -0,0 +1,6 @@
+package org.pizzastore;
+
+public class CheesePizza extends Pizza {
+
+    // each subtype, in this case CheesePizza knows how to prepare itself
+}
--- src/main/java/org/pizzastore/Pizza.java
+++ src/main/java/org/pizzastore/Pizza.java
@@ -2,16 +2,21 @@ package org.pizzastore;
 
 public class Pizza {
 
-
     public void prepare() {
+        System.out.println("Roll the dough.");
+        System.out.println("Put on the sauce.");
+        System.out.println("Add the toppings.");
     }
 
     public void bake() {
+        System.out.println("Put the prepared pizza inside the oven.");
     }
 
     public void cut() {
+        System.out.println("Cut the baked pizza in slices.");
     }
 
     public void box() {
+        System.out.println("Put the sliced pizza inside the box.");
     }
 }
